d3be9ec47a92cf9828acae6e3a40932c7238f587
Sávio <saviopf22@gmail.com>
2025-01-17 00:30:32 -0300

    add list endpoint

--- app/api/urls.py
+++ app/api/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("blogposts/", views.BlogPostCreate.as_view(), name="blogpost-view-create"),
+    path("blogposts/list/", views.BlogPostList.as_view(), name="blogpost-view-list"),
     path(
         "blogposts/<int:pk>/",
         views.BlogPostRetrieveUpdateDestroy.as_view(),
--- app/api/views.py
+++ app/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import BlogPost
 from .serializers import BlogPostSerializer
@@ -19,3 +20,17 @@ class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
     lookup_field = "pk"
+
+
+class BlogPostList(APIView):
+    def get(self, request, format=None):
+        titleParam = request.query_params.get("title", "")
+
+        if titleParam:
+            blog_posts = BlogPost.objects.filter(title__icontains=titleParam)
+        else:
+            blog_posts = BlogPost.objects.all()
+
+        serializer = BlogPostSerializer(blog_posts, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
